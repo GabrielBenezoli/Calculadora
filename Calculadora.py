e34ec26364de3da2763de1463cf238aa5f46fc82
GabrielBenezoli <gabriel.benezoli@hotmail.com>
2024-05-01 15:17:29 -0300

    Atualização e melhoria do projeto
    Essa mudança tem o intuito de melhorar e aprimorar o projeto, que antes tinha como funcionamento apenas as 4 equações básicas de uma calculadora, agora optei por adicionar mais duas, raiz quadrada e potenciação, juntamente com uma nova ordem de estrutura do projeto, agora tendo o questionamento do segundo produto da questão após o questionamento da equação escolhida para ser aplicada na questão.

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,5 @@
-# Questionamento para o usuário escolher os dois valores inicias que serão utilizados na questão.
-a = int(input('Digite o valor do primeiro item da questão:'))
-b = int(input('Digite o valor do segundo item da questão:'))
+# Questionamento para o usuário escolher o valor inicial "a" que será utilizado na questão.
+a = float(input('Digite o valor do primeiro produto da questão:'))
 
 # Def para repetição, caso o usuário escolha uma opção inadequada.
 def again():
@@ -10,76 +9,118 @@ def again():
                '\n 1 - SOMA'
                '\n 2 - SUBTRAÇÃO'
                '\n 3 - DIVISÃO'
-               '\n 4 - MULTIPLICAÇÃO'))
+               '\n 4 - MULTIPLICAÇÃO'
+               '\n 5 - RAIZ.'
+               '\n 6 - POTENCIAÇÃO.'))
  
- # If e else para diferenciar a escolha do usuário e again() para retormar a pergunta caso sua escolha seja fora do pedido.
- if equacao < 1 or equacao > 4:
+ # Variável If para caso a resposta da questão anterior estiver fora do padrão pedido (1 até 6), caso a resposta for menor ou maior que esse pedido, estará incorreta e permanecera dentro desse looping até a resposta ser uma das alternativas pedidas anteriormente.
+ if equacao < 1 or equacao > 6:
     print('Opção invalida, tente novamente')
+
+    # Função "again" para repetir a questão caso a variável If acima for verdadeira.
     again()
 
- else:
+ # Variável if para dar continuidade para as seguintes respostas: (1,2,3,4,6), pois se a escolha da operação for a "5" não irá necessitar de um novo produto na questão.
+ if equacao != 5:
   
-  # Opção match para realizar qualquer operação de acordo com a escolha do usuário. 
-  # Variável string "ex" para compor a respota final, de acordo com a operação escolhida.
+  # Questionamento para o usuário escolher o valor do segundo produto "b" que será utilizado na questão.
+  b = float(input('Digite o valor do segundo produto da questão:'))
+
+ # Variável If para caso a resposta da questão anterior estiver dentro do padrão pedido (1 até 6).
+ # Variável match para dar continuidade da questão e calcular os respectivos valores dos itens de acordo com a operação escolhida anteriormente.
+ # Variável string "ex" para compor a respota final, de acordo com a operação escolhida.
+ if equacao >= 1 or equacao <= 6:
+
   match equacao:
     case 1:
         c = a + b
         ex = 'mais'
+
     case 2:
         c = a - b
         ex = 'menos'
+
     case 3:
         c = a / b
         ex = 'dividido por'
+
     case 4:
         c = a * b
         ex = 'multiplicado por'
 
+    case 5:
+        c = a ** (1/2)
+        print('O calculo da raiz de [',a,'] resulta em: [',c,'].')
+
+    case 6:
+      c = a ** b
+      ex = 'potencializado por'
+
+ # Variável if para dar a resposta "print" da questão para as seguintes respostas: (1,2,3,4,6), sendo que a resposta "print" da opção "5" está no case 5 e não necessitará de outra resposta a baixo.
+ if equacao != 5:
+  
   print('O resultado da equação de [',a,']',ex,'[',b,'] resulta em: [',c,'].')
 
-  # Opção para jogar novamente.
-  nova_questao = int(input('Deseja realizar uma nova questão?'
+ # Opção para realizar uma nova questão.
+ nova_questao = int(input('Deseja realizar uma nova questão?'
                  '\n 1 - SIM'
                  '\n 2 - NÃO'))
-  
-  # Alternativa para uma opção de encerramento da questão.
-  if nova_questao == 2:
+ 
+ # Alternativa para uma opção de encerramento da questão. 
+ if nova_questao == 2:
      print('Fim da questão.')
 
-  # Opção IF e while para continuamento e loping da questão para caso de repetição da questão.
-  if nova_questao == 1:
+ # Opção IF e while para continuamento e loping da questão para caso de repetição da questão.
+ if nova_questao == 1:
     while nova_questao == 1:
      
-     # Opção de anulamento da questão anterior (antiga resposta "c" será 0).
+     # Opção de anulamento da questão anterior (resposta anterior "c" será 0).
      zero = int(input('Deseja zerar a questão?'
                       '\n 1 - SIM'
                       '\n 2 - NÃO'))
      
-     # Opção de anulação da questão anterior.
      if zero == 1:
-        c = 0
-
-     d = int(input('Digite o valor do novo denominador:'))    
+        c = 0   
      
-     nova_equaçao= int(input('Escolha a nova equação que deseja realizar'
+     # Questionamento para o usuário escolher a nova operação que será utilizada.
+     nova_equacao= int(input('Escolha a nova equação que deseja realizar'
                      '\n 1 - ADIÇÃO'
                      '\n 2 - SUBTRAÇÃO'
                      '\n 3 - DIVISÃO'
-                     '\n 4 - MULTIPLICAÇÃO.')) 
+                     '\n 4 - MULTIPLICAÇÃO.'
+                     '\n 5 - RAIZ.'
+                     '\n 6 - POTENCIAÇÃO.')) 
      
-     # If para caso a resposta estiver fora do padrão e While para estar em looping até a resposta estiver no padrão.
-     if nova_equaçao < 1 or nova_equaçao > 4: 
-      while nova_equaçao < 1 or nova_equaçao > 4:
+     # If para caso a resposta estiver fora do padrão e While para estar em looping até a resposta estiver no padrão (1 até 6).
+     if nova_equacao < 1 or nova_equacao > 6: 
+      while nova_equacao < 1 or nova_equacao > 6:
        
        print('Opção invalida, tente novamente')
-       nova_equaçao= int(input('Escolha a nova equação que deseja realizar'
+       nova_equacao= int(input('Escolha a nova equação que deseja realizar'
                      '\n 1 - ADIÇÃO'
                      '\n 2 - SUBTRAÇÃO'
                      '\n 3 - DIVISÃO'
-                     '\n 4 - MULTIPLICAÇÃO.')) 
-        
-     # Opção match para realizar qualquer operação de acordo com a escolha do usuário.    
-     match nova_equaçao:
+                     '\n 4 - MULTIPLICAÇÃO.'
+                     '\n 5 - RAIZ.'
+                     '\n 6 - POTENCIAÇÃO.')) 
+
+     # Variável If para dar continuidade caso a escolha da operação ser a "5", juntamente com a opção de zerar a questão anterior for positiva "1".
+     # Se ambas respostas forem de acordo com a variável, então a operação irá necessitar de um novo produto "c".
+     if nova_equacao == 5 and zero == 1:
+       c = int(input('Digite o valor do novo produto:')) 
+
+     # Variável Elif para caso a escolha da operação ser a "6", juntamente com a opção de zerar a questão anterior "1" for positiva.
+     #Se ambas as respotas forem de acordo com a variável, a nova operação "6" (potênciaçaõ), irá necessitar de um novo produto base "c" e um novo produto para ser seu expoente "d".
+     elif nova_equacao == 6 and zero == 1:
+       c = int(input('Digite o valor do produto base da equação:'))
+       d = int(input('Digite o valor do expoente desse produto:'))
+     
+     # Variável Elif para caso a escolha de operação estiver dentro do padrão (1,2,3,4,6), pois todas essas alternativas irão necessitar de um novo produto e a alternativa "c" irá necessitar de um novo expoente, caso a opção zero for igual à: "2".
+     elif nova_equacao != 5: 
+       d = int(input('Digite o valor do novo denominador:'))
+
+     # Opção match igual à anterior, mudando apenas suas variáveis "c" e "d", juntamente com a nova resposta da questão "e".    
+     match nova_equacao:
         case 1:
            e = c + d
            ex = 'mais'
@@ -95,12 +136,22 @@ def again():
         case 4:
            e = c * d
            ex = 'multiplicado por'
-                           
-     print('O resultado da equação de [',c,']',ex,'[',d,'] resulta em: [',e,'].')
+
+        case 5:
+         e = c ** (1/2)
+         print('O calculo da raiz quadrada de: [',c,'] resulta em: [',e,'].')
+
+        case 6:
+         e = c ** d
+         ex = 'potencializado por'
+            
+     if nova_equacao != 5:                     
+      print('O resultado da equação de [',c,']',ex,'[',d,'] resulta em: [',e,'].')
 
      # Mudança de valor da resposta anterior para seguir de acordo com o funcionamento da questão.
      c = e
 
+     # Opção para realizar uma nova questão.
      nova_questao = int(input('Deseja realizar uma nova equação?'
                     '\n 1 - SIM'
                     '\n 2 - NÃO'))
@@ -108,5 +159,6 @@ def again():
      # Finalização da questão.
      if nova_questao == 2:
         print('Fim da equação.')
-     
+
+# Função "again" para realizar a questão.     
 again()
